09e0b03c2dc021464622419d9a41685166ff0ee0
Andrii <andriy07da@gmail.com>
2026-07-14 14:14:14 +0300

    Change PaginationResultDto field name

--- DogTrainerTestTask/Dto/PaginationResultDto.cs
+++ DogTrainerTestTask/Dto/PaginationResultDto.cs
@@ -1,3 +1,3 @@
 ﻿namespace DogTrainerTestTask.Dto;
 
-public record PaginationResultDto<TItem>(IReadOnlyList<TItem> Item, int PageNumber, int PageSize, int TotalCount);
+public record PaginationResultDto<TItem>(IReadOnlyList<TItem> Items, int PageNumber, int PageSize, int TotalCount);
